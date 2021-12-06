b7415c44a616fa3659ca7bd181f0f46b32bf508f
Jezell <jzlldomer@gmail.com>
2021-12-06 22:32:10 +0800

    Fixing the loop and the conditions

--- prg2GuessTheNumber.py
+++ prg2GuessTheNumber.py
@@ -5,26 +5,28 @@
 # Display “Less than” if the inputed number is less than the random number
 # Repeat asking the user until the random number has been guessed correctly.
 
-import random
 
-print ("GUESS THE NUMBER")
+print ("╔════════════════════════════════╗\n   Welcome to GUESS THE NUMBER!\n╚════════════════════════════════╝")
 
 
 import random
 def gtn():
     ans = random.randint(0, 100)
-    guess = int(input("Enter an integer from 0 to 100: "))
+    print ("Are you ready to guess? Let's go!\n ")
+    guess = int(input("\033[1;35;40mEnter an integer from 0 to 100: "))
+    total = 0
     while ans != "guess":
-        print
+        
         if guess < ans:
-            print ("less than")
-            guess = int(input("Enter an integer from 0 to 100: "))
+            total += 1
+            print ("\n\033[1;36;40mLess than!")
+            guess = int(input("\n\033[1;35;40mEnter an integer from 0 to 100: "))
         elif guess > ans:
-            print ("greater than")
-            guess = int(input("Enter an integer from 0 to 100: "))
+            total += 1
+            print ("\n\033[1;34;40mGreater than!")
+            guess = int(input("\n\033[1;35;40mEnter an integer from 0 to 100: "))
         else:
-            print ("you guessed it!")
+            print (f"\n\033[1;32;40mCongrats, you guessed it! The number is \033[0;33;40m[ {ans} ] \033[1;32;40mand you used \033[0;33;40m[ {total} ] \033[1;32;40mattempts 😇 ")
             break
-    print (ans)
 
 gtn()
