299ea121af17d06dcd32495870fb00ecec3797d1
Jezell <jzlldomer@gmail.com>
2021-12-06 22:59:56 +0800

    Adding colors and fixing some codes

--- prg2GuessTheNumber.py
+++ prg2GuessTheNumber.py
@@ -6,27 +6,29 @@
 # Repeat asking the user until the random number has been guessed correctly.
 
 
-print ("╔════════════════════════════════╗\n   Welcome to GUESS THE NUMBER!\n╚════════════════════════════════╝")
+print ("\033[0;33;40m╔════════════════════════════════╗\n   \033[1;37;40mWelcome to GUESS THE NUMBER!\n\033[0;33;40m╚════════════════════════════════╝")
 
 
 import random
 def gtn():
     ans = random.randint(0, 100)
-    print ("Are you ready to guess? Let's go!\n ")
-    guess = int(input("\033[1;35;40mEnter an integer from 0 to 100: "))
+    print ("\n\033[0;35;40mAre you ready to guess? Let's go!\n\033[1;33;40m*\033[1;37;40m.·:·..·:·.\033[1;33;40m☽✧    ✦    \033[1;33;40m✧☾\033[1;37;40m.·:·..·:·.\033[1;33;40m*\n\033[1;36;40m ")
+    guess = int(input("\033[1;35;40mEnter an integer from 0 to 100:\n»•» "))
     total = 0
     while ans != "guess":
         
         if guess < ans:
             total += 1
-            print ("\n\033[1;36;40mLess than!")
-            guess = int(input("\n\033[1;35;40mEnter an integer from 0 to 100: "))
+            print ("\033[1;36;40m\n[ Less than! ]\n\n\033[1;33;40m*\033[1;37;40m.·:·..·:·.\033[1;33;40m☽✧    ✦    \033[1;33;40m✧☾\033[1;37;40m.·:·..·:·.\033[1;33;40m*\n\033[1;36;40m")
+            guess = int(input("\033[1;35;40mEnter an integer from 0 to 100:\n»•» "))
         elif guess > ans:
             total += 1
-            print ("\n\033[1;34;40mGreater than!")
-            guess = int(input("\n\033[1;35;40mEnter an integer from 0 to 100: "))
+            print ("\033[1;34;40m\n[ Greater than! ]\n\n\033[1;33;40m*\033[1;37;40m.·:·..·:·.\033[1;33;40m☽✧    ✦    \033[1;33;40m✧☾\033[1;37;40m.·:·..·:·.\033[1;33;40m*\n\033[1;36;40m")
+            guess = int(input("\033[1;35;40mEnter an integer from 0 to 100:\n»•» "))
         else:
-            print (f"\n\033[1;32;40mCongrats, you guessed it! The number is \033[0;33;40m[ {ans} ] \033[1;32;40mand you used \033[0;33;40m[ {total} ] \033[1;32;40mattempts 😇 ")
+            print ("\n\033[1;33;40m*\033[1;37;40m.·:·..·:·.\033[1;33;40m☽✧    ✦    \033[1;33;40m✧☾\033[1;37;40m.·:·..·:·.\033[1;33;40m*\n\033[1;36;40m")
+            print (f"\n\033[1;32;40mCongrats, you guessed it! \nThe number is \033[0;33;40m[ {ans} ]😱 \033[1;32;40mand you used \033[0;33;40m[ {total} ]😇 \033[1;32;40mattempts to guess the number\n ")
+            print ("\033[0;32;40m==.==.==.==.==.==.==.==.exit==.==.==.==.==.==.==.==.\n")
             break
 
 gtn()
